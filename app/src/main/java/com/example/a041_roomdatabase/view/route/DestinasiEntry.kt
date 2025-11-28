7dc9fcce7028e1ad5fb15e29e11fb30126133811
lilalr <gurennut@gmail.com>
2025-11-28 19:39:56 +0700

    membuat objek Destinasi Entry

---
+++ app/src/main/java/com/example/a041_roomdatabase/view/route/DestinasiEntry.kt
@@ -0,0 +1,9 @@
+package com.example.a041_roomdatabase.view.route
+
+import com.example.a041_roomdatabase.R
+import com.example.a041_roomdatabase.view.route.DestinasiNavigasi
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
