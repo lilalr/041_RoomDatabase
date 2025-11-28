20bd9959dd808fa542984351ff51eb8e651cf993
lilalr <gurennut@gmail.com>
2025-11-28 19:43:53 +0700

    membuat class EntryViewModel

---
+++ app/src/main/java/com/example/a041_roomdatabase/viewmodel/EntryViewModel.kt
@@ -0,0 +1,14 @@
+package com.example.a041_roomdatabase.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import com.example.a041_roomdatabase.repositori.RepositoriSiswa
+import com.example.a041_roomdatabase.room.Siswa
+
+class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel()
+
+
+
+
