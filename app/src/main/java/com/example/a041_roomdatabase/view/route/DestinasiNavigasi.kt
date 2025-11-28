77bb11335851bc14e78dc4f5c372f3f3097c664a
lilalr <gurennut@gmail.com>
2025-11-28 19:40:21 +0700

    membuat interface Destinasi Navigasi

---
+++ app/src/main/java/com/example/a041_roomdatabase/view/route/DestinasiNavigasi.kt
@@ -0,0 +1,7 @@
+package com.example.a041_roomdatabase.view.route
+
+interface DestinasiNavigasi {
+    val route: String /* nama unik utk nentuin jalur composable*/
+    /* string resource id yg berisi judul yang akan ditampilkan di layar*/
+    val titleRes: Int
+}
