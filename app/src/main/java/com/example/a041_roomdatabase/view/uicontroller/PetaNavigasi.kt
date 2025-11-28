6e064f99ea9606cc0a603346fcdb7b832ce0281f
lilalr <gurennut@gmail.com>
2025-11-28 19:51:30 +0700

    impelement HostNavigasi dengan NavHost

--- app/src/main/java/com/example/a041_roomdatabase/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/a041_roomdatabase/view/uicontroller/PetaNavigasi.kt
@@ -17,3 +17,25 @@ import com.example.a041_roomdatabase.view.route.DestinasiHome
 fun SiswaApp(navController: NavHostController= rememberNavController(), modifier: Modifier){
     HostNavigasi(navController = navController)
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+){
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ) {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+            )
+        }
+        composable(DestinasiEntry.route) {
+            EntrySiswaScreen(navigateBack = { navController.popBackStack() })
+        }
+    }
+}
