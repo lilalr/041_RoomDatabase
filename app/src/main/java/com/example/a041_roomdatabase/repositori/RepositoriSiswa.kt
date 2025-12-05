7baaf079f27e3be311ae41aca65b115ff58a92a3
lilalr <gurennut@gmail.com>
2025-12-05 14:57:46 +0700

    menambah implementasi getSiswaStream, deleteSiswa, dan updateSiswa

--- app/src/main/java/com/example/a041_roomdatabase/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/a041_roomdatabase/repositori/RepositoriSiswa.kt
@@ -20,3 +20,8 @@ class OfflineRepositoriSiswa(
     override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
     override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
 
+    //edit 2: tambah 3 override berikut
+    override fun getSiswaStream(id: Int): Flow<Siswa?> = siswaDao.getSiswa(id)
+    override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
+    override suspend fun updateSiswa(siswa: Siswa) = siswaDao.update(siswa)
+}
