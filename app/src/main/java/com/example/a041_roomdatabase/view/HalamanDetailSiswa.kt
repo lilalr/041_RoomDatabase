f49b67179b51cf00fe61c6f8a011fa01df21fc8f
lilalr <gurennut@gmail.com>
2025-12-05 15:31:48 +0700

    menambah DeleteConfirmationDialog untuk konfirmasi penghapusan siswa

--- app/src/main/java/com/example/a041_roomdatabase/view/HalamanDetailSiswa.kt
+++ app/src/main/java/com/example/a041_roomdatabase/view/HalamanDetailSiswa.kt
@@ -112,3 +112,16 @@ private fun BodyDetailDataSiswa(
         ) {
             Text(stringResource(R.string.delete))
         }
+        if (deleteConfirmationRequired) {
+            DeleteConfirmationDialog(
+                onDeleteConfirm = {
+                    deleteConfirmationRequired = false
+                    onDelete()
+                },
+                onDeleteCancel = { deleteConfirmationRequired = false },
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_medium))
+            )
+        }
+    }
+}
+
