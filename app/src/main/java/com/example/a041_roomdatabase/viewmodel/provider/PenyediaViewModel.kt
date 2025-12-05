c14fcd97a4f73800666de1c27864ee756c997837
lilalr <gurennut@gmail.com>
2025-12-05 16:54:50 +0700

    menambah code penyedia view model

--- app/src/main/java/com/example/a041_roomdatabase/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/a041_roomdatabase/viewmodel/provider/PenyediaViewModel.kt
@@ -1,12 +1,14 @@
 package com.example.a041_roomdatabase.viewmodel.provider
 
-import android.app.Application
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.a041_roomdatabase.repositori.AplikasiSiswa
+import com.example.a041_roomdatabase.viewmodel.DetailViewModel
 import com.example.a041_roomdatabase.viewmodel.EntryViewModel
+import com.example.a041_roomdatabase.viewmodel.HomeViewModel
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
@@ -17,6 +19,15 @@ object PenyediaViewModel {
         initializer {
             EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
         }
+        //edit: tambah initializer utk DetailViewModel dan EditViewModel
+        initializer {
+            DetailViewModel (this.createSavedStateHandle(),
+                aplikasiSiswa().container.repositoriSiswa)
+        }
+//        initializer {
+//            EditViewModel(this.createSavedStateHandle(),
+//                aplikasiSiswa().container.repositoriSiswa)
+//        }
     }
 }
 
